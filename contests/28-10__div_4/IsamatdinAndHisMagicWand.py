dd6392052576918d1d83e49b15ff15374786d6cc
Pranav C R <pranavcr001@gmail.com>
2025-10-30 00:07:17 +0530

    solve contest magicwand question

---
+++ contests/28-10__div_4/IsamatdinAndHisMagicWand.py
@@ -0,0 +1,9 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    has_odd=any(x%2==1 for x in a)
+    has_even=any(x%2==0 for x in a)
+    if has_odd and has_even:
+        a.sort()
+    print(*a)

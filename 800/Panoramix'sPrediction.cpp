aa4f3094182c6dc44a6b28be12c77907aae4077c
Pranav C R <pranavcr001@gmail.com>
2025-12-07 12:20:12 +0530

    Create Panoramix'sPrediction.cpp

---
+++ 800/Panoramix'sPrediction.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x){
+    if(x<2)return false;
+    for(int i=2;i*i<=x;i++){
+        if(x%i==0)return false;
+    }
+    return true;
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int printed=0;
+    for(int i=n+1;i<=m;i++){
+        if(isPrime(i)){
+            if(i==m)cout<<"YES";
+            else cout<<"NO";
+            printed=1;
+            break;
+        }
+    }
+    if (printed==0)
+        cout<<"NO";
+    return 0;
+}

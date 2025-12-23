601c43f98c6cc36cdd13b41acc68554fa81751c1
Pranav C R <pranavcr001@gmail.com>
2025-12-23 10:04:29 +0530

    solve ICPC balloons and Spell Check

--- 800/SpellCheck.cpp
+++ 800/SpellCheck.cpp
@@ -8,17 +8,16 @@ int main(){
     cin>>t;
     while(t--){
         int n;
-        string s;
         cin>>n;
+        string s;
         cin>>s;
-
-        set<char> st;
-        for(int i=0;i<s.size();i++){
-            st.insert(s[i]);
+        sort(s.begin(),s.end());
+        if (s=="Timru"){
+            cout<<"yes"<<endl;
         }
-
-        int k=st.size();
-        cout<<n+k<<endl;
+        else{
+            cout<<"no"<<endl;
+        }
     }
     return 0;
 }

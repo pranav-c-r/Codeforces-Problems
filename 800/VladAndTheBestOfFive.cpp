4bfd627949afd5273790bd9b812714daac24926b
Pranav C R <pranavcr001@gmail.com>
2026-01-06 00:11:36 +0530

    Create VladAndTheBestOfFive.cpp

---
+++ 800/VladAndTheBestOfFive.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int countA=0,countB=0;
+        for (char c:s){
+            if(c=='A') countA++;
+            else countB++;
+        }
+        if(countA>countB) cout<<'A'<<endl;
+        else cout<<'B'<<endl;
+    }
+    return 0;
+}

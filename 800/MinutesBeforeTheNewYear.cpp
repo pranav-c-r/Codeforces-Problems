14a89d3971cff0d5e78e442d4484a4d46f49776c
Pranav C R <pranavcr001@gmail.com>
2026-01-14 11:53:55 +0530

    Create MinutesBeforeTheNewYear.cpp

---
+++ 800/MinutesBeforeTheNewYear.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int h,m;
+        cin>>h>>m;
+        int total=24*60;
+        int passed=h*60+m;
+        int minutes=total-passed;
+        cout<<minutes<<endl;
+    }
+    return 0;
+}

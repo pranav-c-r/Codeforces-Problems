440cfeb3cd59eda823e341fc430ec124cb901e9d
Pranav C R <pranavcr001@gmail.com>
2026-01-23 19:18:38 +0530

    Create PlusOneOnSubset.cpp

---
+++ 800/PlusOneOnSubset.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++) {
+            cin>>a[i];
+        }
+        int mn=*min_element(a.begin(),a.end());
+        int mx=*max_element(a.begin(),a.end());
+        cout<<mx-mn<<endl;
+    }
+    return 0;
+}
